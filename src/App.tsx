800133703acf137fae8218e50ec4b6bdfd5731ce
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2024-05-28 21:53:20 -0500

    template init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+ return (
+  <h1 className="prose prose-2xl">
+    Hello world!
+  </h1>
+ )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography'), daisyui],
+  daisyui: {
+    themes: ["light", "dark", "business"],
+  },
+}
+
